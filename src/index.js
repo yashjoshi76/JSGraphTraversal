fc556d22e1849a2ca1ce00eeb684ff249d3e0c98
Yash Joshi <yash.joshi76@gmail.com>
2022-06-15 13:52:33 +0100

    recursive approach

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-const dfs = (graph, source) => {
-  const stack = [source];
+// const dfs = (graph, source) => {
+//   const stack = [source];
 
-  while (stack.length > 0) {
-    const current = stack.pop();
-    console.log(current);
-    for (let i of graph[current]) {
-      stack.push(i);
-    }
+//   while (stack.length > 0) {
+//     const current = stack.pop();
+//     console.log(current);
+//     for (let i of graph[current]) {
+//       stack.push(i);
+//     }
+//   }
+// };
+
+//recursive
+const dfs = (graph, source) => {
+  console.log(source);
+  for (let i of graph[source]) {
+    dfs(graph, i);
   }
 };
 const graph = {
